f6f2087d603afea6d7df93c148fecfa638767966
Archer Scherer <ascherer831@gmail.com>
2021-04-24 10:56:57 -0500

    intermediateFoundations DONE'!'

--- JavaScript/intermediateFoundations/intermediateFoundations.js
+++ JavaScript/intermediateFoundations/intermediateFoundations.js
@@ -20,16 +20,16 @@
 
 // Fibonacci - Create a function to generate Fibonacci numbers.  In this famous mathematical sequence, each number is the sum of the previous two, starting with values 0 and 1.  Your function should accept one argument, an index into the sequence (where 0 corresponds to the initial value, 4 corresponds to the value four later, etc).  Examples: fibonacci(0) = 0 (given), fibonacci(1) = 1 (given), fibonacci(2) = 1 (fib(0)+fib(1), or 0+1), fibonacci(3) = 2 (fib(1) + fib(2)3, or 1+1), fibonacci(4) = 3 (1+2), fibonacci(5) = 5 (2+3), fibonacci(6) = 8 (3+5), fibonacci(7) = 13 (5+8).  
 
-function fibonacciNums(num) {
-    // var fibNum= ;
-    var fibArr= [0,1];
-    for(var i=2; i<= num; i++) {
-        fibArr[i]= fibArr[i-1] + fibArr[i-2];
-        // fibArr.push(fibArr[i])
-    }
-    return fibArr[num];
-}
-console.log(fibonacciNums(10));
+// function fibonacciNums(num) {
+//     // var fibNum= ;
+//     var fibArr= [0,1];
+//     for(var i=2; i<= num; i++) {
+//         fibArr[i]= fibArr[i-1] + fibArr[i-2];
+//         // fibArr.push(fibArr[i])
+//     }
+//     return fibArr[num];
+// }
+// console.log(fibonacciNums(100));
 
 // Array: Second-to-Last: Return the second-to-last element of an array. Given [42, true, 4, "Liam", 7], return "Liam".  If array is too short, return null.
 
